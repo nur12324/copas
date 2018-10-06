144e4bbd2e17d78702c30b3cc35400e4419e7b85
nur12324 <43899145+nur12324@users.noreply.github.com>
2018-10-06 11:59:38 +0700

    Update db.php

--- www/db.php
+++ www/db.php
@@ -1,4 +1,4 @@
 <?php
  header("Access-Control-Allow-Origin: *");
- $con = mysqli_connect("localhost","root","root","phonegap_demo") or die ("could not connect database");
+ $con = mysqli_connect("localhost","id7374459_nur12324","deviljin10","id7374459_ahmad) or die ("could not connect database");
 ?>
